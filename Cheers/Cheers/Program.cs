1e321628508bbddab270ad2bd8db3b4ad883786a
Ryan Tanay <ryantanay@newevolution.org>
2015-10-13 15:00:53 -0500

    Accepting input, parsing to lower case & choosing correct article

---
+++ Cheers/Cheers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there! What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+            name = name.ToLower();
+            foreach (char letter in name)
+            {
+                string aOrAn = "a";
+                foreach (char vowel in "aeiouy")
+                {
+                    if (letter == vowel)
+                    {
+                        aOrAn = "an";
+                    }
+                }
+                Console.WriteLine("Give me " + aOrAn + " " + letter);
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
